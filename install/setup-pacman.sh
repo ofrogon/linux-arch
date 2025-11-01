cf072618fe206001be7fda6a9170375385980b71
Alexandre Beaudoin <abeaudoin@coldmechanics.com>
2025-11-01 19:19:24 -0400

    Add more sudo call in the packman configuration

--- install/setup-pacman.sh
+++ install/setup-pacman.sh
@@ -15,13 +15,13 @@ require_root() {
 # Enable multilib repository
 enable_multilib() {
   echo "Enabling [multilib] repository..."
-  sed -i '/^\s*#\[multilib\]/,/^\s*#Include/ s/^\s*#//' "$PACMAN_CONF"
+  sudo sed -i '/^\s*#\[multilib\]/,/^\s*#Include/ s/^\s*#//' "$PACMAN_CONF"
 }
 
 # Enable color
 enable_color() {
   echo "Enabling color output..."
-  sed -i 's/^#Color/Color/' "$PACMAN_CONF"
+  sudo sed -i 's/^#Color/Color/' "$PACMAN_CONF"
 }
 
 # Enable pacman loading bar (ILoveCandy), progress bar, verbose pkg list
@@ -30,11 +30,11 @@ enable_candy_and_bar() {
 
   # Add ILoveCandy if not already present
   if ! grep -q "^[[:space:]]*ILoveCandy" "$PACMAN_CONF"; then
-    sed -i '/^\[options\]/a ILoveCandy' "$PACMAN_CONF"
+    sudo sed -i '/^\[options\]/a ILoveCandy' "$PACMAN_CONF"
   fi
 
   # Enable ParallelDownloads and VerbosePkgLists
-  sed -i 's/^#ParallelDownloads/ParallelDownloads/' "$PACMAN_CONF"
+  sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' "$PACMAN_CONF"
 }
 
 main() {
